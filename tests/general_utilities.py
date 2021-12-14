d5d061a67c0592e91c952fb5cc4a42a6b87ea87f
piotr.idzik <vil02@o2.pl>
2021-12-14 19:18:30 +0100

    more test strings

--- tests/general_utilities.py
+++ tests/general_utilities.py
@@ -71,7 +71,9 @@ def get_test_string_list():
         '\n'.join(['Line 1', 'Line 2']),
         '',
         '#',
-        'aaa\tb\nccc\t#']
+        'aaa\tb\nccc\t#',
+        '\'',
+        '\"']
 
 
 def check_all(in_proc_single_fun):
