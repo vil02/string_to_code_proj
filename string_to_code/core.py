8436fe13f0472422ac311c44356c71b106c569a6
piotr.idzik <vil02@o2.pl>
2022-01-27 18:00:34 +0100

    style: linting

--- string_to_code/core.py
+++ string_to_code/core.py
@@ -8,20 +8,30 @@ Atom = collections.namedtuple('Atom', 'atom_char')
 
 
 class SimpleFunction:
+    """
+    represents a function with no arguments and no return value calling
+    other functions of such type or displaying single characters
+    """
     def __init__(self, in_function_name, in_called_list):
         self._function_name = in_function_name
         self._called_list = in_called_list
 
     @property
     def function_name(self):
+        """returns the name of the function represented by this object"""
         return self._function_name
 
     @property
     def called_list(self):
+        """
+        returns the list of functions called by function
+        represented by this object
+        """
         return self._called_list
 
 
 def str_pieces(in_str, in_pieces_len):
+    """returns in_str splited into pieces of lengths as in in_pieces_len"""
     assert all(_ > 0 for _ in in_pieces_len)
     assert sum(in_pieces_len) == len(in_str)
     cur_str = in_str
@@ -34,6 +44,7 @@ def str_pieces(in_str, in_pieces_len):
 
 
 def random_pieces_len(in_total_len):
+    """returns a list of positive numbers wuch their sum being in_total_len"""
     cur_num = in_total_len
     res = []
     while cur_num > 0:
@@ -46,10 +57,12 @@ def random_pieces_len(in_total_len):
 
 
 def random_split(in_str):
+    """randomply splits in_str"""
     return str_pieces(in_str, random_pieces_len(len(in_str)))
 
 
 def gen_function_names(in_name_prefix='f_'):
+    """yields 'f_0', 'f_1', 'f_2', ..."""
     cur_function_num = 0
     while True:
         yield f"{in_name_prefix}{cur_function_num}"
@@ -57,6 +70,7 @@ def gen_function_names(in_name_prefix='f_'):
 
 
 def str_to_function_stack(in_str, function_names):
+    """returns a SimpleFunction object which evaluates to in_str."""
     function_stack = []
     known_codes = {}
 
