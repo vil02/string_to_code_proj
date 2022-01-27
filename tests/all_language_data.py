44f98b85720b65e9c1f92c5adf5e45dbd340eb57
piotr.idzik <vil02@o2.pl>
2022-01-27 17:15:10 +0100

    style: add missing docstrings

--- tests/all_language_data.py
+++ tests/all_language_data.py
@@ -1,3 +1,6 @@
+"""
+imports all of the setup_*.py modules
+"""
 import setup_bash
 import setup_cpp
 import setup_python
@@ -5,9 +8,11 @@ import setup_lisp
 
 
 def get_all_test_data():
+    """returns test-specific-descrition of each available langage"""
     return [_.get_test_data()
             for _ in [setup_cpp, setup_bash, setup_python, setup_lisp]]
 
 
 def get_all_ids():
+    """returns id's of all of available languages"""
     return (_.id for _ in get_all_test_data())
