723444930832456b095566afb55201815cac8c2d
piotr.idzik <vil02@o2.pl>
2022-01-30 14:07:30 +0100

    feat: add basic examples

---
+++ examples/example_to_cpp.py
@@ -0,0 +1,9 @@
+"""Exaple usage of string_to_code.to_cpp"""
+
+import setup_examples  # noqa # pylint: disable=unused-import
+from string_to_code import to_cpp
+
+EXAMPLE_STR = 'Hello World!'
+CODE = to_cpp.proc(EXAMPLE_STR)
+
+print(f'C++ code below prints \'{EXAMPLE_STR}\':\n{CODE}')
