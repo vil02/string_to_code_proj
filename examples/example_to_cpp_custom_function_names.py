f76506e461bcc2bb49416799c81835f4b8fa93c8
piotr.idzik <vil02@o2.pl>
2022-01-30 14:07:30 +0100

    feat: add basic examples

---
+++ examples/example_to_cpp_custom_function_names.py
@@ -0,0 +1,11 @@
+"""Exaple usage of string_to_code.to_cpp with custom function names"""
+
+import itertools
+import setup_examples  # noqa # pylint: disable=unused-import
+from string_to_code import to_cpp
+
+EXAMPLE_STR = 'messy code'
+FUNCTION_NAMES = (f"some_messy_fun_num_{_}" for _ in itertools.count(100, 100))
+CODE = to_cpp.proc(EXAMPLE_STR, FUNCTION_NAMES)
+
+print(f'C++ code below prints \'{EXAMPLE_STR}\':\n{CODE}')
---
+++ examples/setup_examples.py
@@ -0,0 +1,8 @@
+"inserts project's rooth path to sys.path"
+
+import pathlib
+import sys
+
+_PROJ_PATH_STR = str(pathlib.Path(__file__).parents[1])
+assert str(_PROJ_PATH_STR).endswith('string_to_code_proj')
+sys.path.insert(0, _PROJ_PATH_STR)
