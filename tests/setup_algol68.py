04c01048a9d806274b3aa2f692965cbc6ebcf6ea
Piotr Idzik <vil02@o2.pl>
2022-04-16 13:47:58 +0200

    chore: add algol68 tests setup

---
+++ tests/setup_algol68.py
@@ -0,0 +1,40 @@
+"""
+setup for the tests of the module string_to_ALGOL 68
+"""
+import subprocess
+import general_utilities as gu
+
+from string_to_code import to_algol68
+
+
+def get_algol68_interpreter():
+    """returns the name of the ALGOL 68 interpreter"""
+    return 'a68g'
+
+
+def run_algol68_code(in_code, tmp_folder):
+    """
+    Runs the ALGOL 68 code in_code.
+    Returns the output of the program.
+    """
+    source_filename = gu.get_unique_filename(tmp_folder, 'alg')
+    gu.save_str_to_file(tmp_folder/source_filename, in_code)
+
+    res = subprocess.run(
+        [get_algol68_interpreter(),
+         '--pedantic',
+         source_filename],
+        cwd=str(tmp_folder),
+        check=True,
+        capture_output=True,
+        text=True)
+    return res
+
+
+def get_test_data():
+    """returns test data for the module string_to_ALGOL 68"""
+    return gu.Language(
+        [get_algol68_interpreter()],
+        to_algol68.proc,
+        run_algol68_code,
+        'ALGOL 68')
