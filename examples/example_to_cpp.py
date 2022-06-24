a85ed668eea34103423477fa44f30dc0380d62d3
piotr.idzik <vil02@o2.pl>
2022-06-24 16:18:38 +0200

    style: add missing comma

--- examples/example_to_cpp.py
+++ examples/example_to_cpp.py
@@ -3,7 +3,7 @@
 import setup_examples  # noqa # pylint: disable=unused-import
 from string_to_code import to_cpp
 
-EXAMPLE_STR = 'Hello World!'
+EXAMPLE_STR = 'Hello, World!'
 CODE = to_cpp.proc(EXAMPLE_STR)
 
 print(f'C++ code below prints \'{EXAMPLE_STR}\':\n{CODE}')
--- examples/example_to_java.py
+++ examples/example_to_java.py
@@ -3,7 +3,7 @@
 import setup_examples  # noqa # pylint: disable=unused-import
 from string_to_code import to_java
 
-EXAMPLE_STR = 'Hello World!'
+EXAMPLE_STR = 'Hello, World!'
 CODE = to_java.proc(EXAMPLE_STR, class_name='HelloWorldPrinter')
 
 print(f'java code below prints \'{EXAMPLE_STR}\':\n{CODE}')
