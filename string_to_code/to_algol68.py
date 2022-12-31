194cb8b8394058c1e65c0d538590d078483a8ff6
“vil02” <vil02@o2.pl>
2022-12-31 13:44:09 +0100

    refactor: add proc_printer_program and use utils in to_algol68

--- string_to_code/to_algol68.py
+++ string_to_code/to_algol68.py
@@ -2,6 +2,7 @@
 provides string_to_algol68 utilities
 """
 from . import core
+from . import utils
 
 
 def atom_to_code(in_atom):
@@ -24,6 +25,13 @@ def function_call_str(in_function_name):
     return f"{in_function_name};"
 
 
+def _call_function_or_atom(in_data):
+    if isinstance(in_data, core.Atom):
+        return atom_to_code(in_data)
+    assert isinstance(in_data, core.SimpleFunction)
+    return function_call_str(in_data.function_name)
+
+
 def function_to_code(in_function):
     """
     returns a string representing the code of the function/procedure
@@ -31,43 +39,29 @@ def function_to_code(in_function):
     """
     assert isinstance(in_function, core.SimpleFunction)
 
-    def proc_single_body_line(in_line_data):
-        if isinstance(in_line_data, core.Atom):
-            res = atom_to_code(in_line_data)
-        else:
-            res = function_call_str(in_line_data.function_name)
-        return "  " + res
-
     function_body = "\n".join(
-        proc_single_body_line(_) for _ in in_function.called_list
+        "  " + _call_function_or_atom(_) for _ in in_function.called_list
     )
-    if function_body[-1] == ";":
-        function_body = function_body[:-1]
+    if function_body:
+        assert function_body[-1] == ";"
+        function_body = "\n" + function_body[:-1] + "\n"
 
-    return "\n".join(
-        [f"PROC {in_function.function_name} = VOID :(", function_body, ");"]
-    )
+    return f"PROC {in_function.function_name} = VOID :({function_body});"
 
 
-def proc(in_str, gen_function_names=None):
-    """
-    returns an ALGOL 68 code printing in_str to the standard output
-    """
-    if gen_function_names is None:
-        gen_function_names = core.gen_function_names("p")
+def _main_call_to_code(in_initial_call):
+    if in_initial_call is None:
+        return 'print("")'
+    res = _call_function_or_atom(in_initial_call)
+    assert res[-1] == ";"
+    return res[:-1]
 
-    res = 'print("")\n'
-    if in_str:
-        printer_program = core.get_printer_program(in_str, gen_function_names)
-        function_definitions = printer_program.needed_function_definitions_str(
-            function_to_code, "\n\n\n"
-        )
-        if function_definitions:
-            function_definitions = function_definitions + "\n\n"
-        main_call_str = printer_program.initial_call_str(
-            atom_to_code, function_call_str
-        )
-        main_call_str = "\n" + main_call_str[0:-1] + "\n"
 
-        res = function_definitions + main_call_str
-    return res
+def _join_to_final(main_call, function_definitions):
+    assert main_call[-1] != "\n"
+    return "\n\n\n".join(function_definitions + [main_call + "\n"])
+
+
+proc_printer_program, proc = utils.get_all_proc_functions(
+    _main_call_to_code, function_to_code, _join_to_final
+)
