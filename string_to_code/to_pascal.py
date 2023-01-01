2429b1fdb2e6ead3c7d6b2e2f27dfde5afb45478
“vil02” <vil02@o2.pl>
2023-01-01 20:39:06 +0100

    refactor: use utils

--- string_to_code/to_pascal.py
+++ string_to_code/to_pascal.py
@@ -2,6 +2,7 @@
 provides string_to_pascal utilities
 """
 from . import core
+from . import utils
 
 
 def atom_to_code(in_atom):
@@ -25,6 +26,11 @@ def function_call_str(in_function_name):
     return f"{in_function_name}();"
 
 
+_call_function_or_atom = utils.get_call_function_or_atom(
+    atom_to_code, function_call_str
+)
+
+
 def function_to_code(in_function):
     """
     returns a string representing the code of the function/procedure
@@ -32,15 +38,8 @@ def function_to_code(in_function):
     """
     assert isinstance(in_function, core.SimpleFunction)
 
-    def proc_single_body_line(in_line_data):
-        if isinstance(in_line_data, core.Atom):
-            res = atom_to_code(in_line_data)
-        else:
-            res = function_call_str(in_line_data.function_name)
-        return "  " + res
-
     function_body = "\n".join(
-        proc_single_body_line(_) for _ in in_function.called_list
+        "  " + _call_function_or_atom(_) for _ in in_function.called_list
     )
     if function_body:
         assert function_body[-1] == ";"
@@ -56,34 +55,29 @@ def function_to_code(in_function):
     )
 
 
-def proc_printer_program(in_printer_program):
-    function_definitions = in_printer_program.needed_function_definitions_str(
-        function_to_code, "\n\n\n"
+def _main_call_to_code(in_initial_call):
+    return (
+        ""
+        if in_initial_call is None
+        else "\n  " + _call_function_or_atom(in_initial_call)[:-1]
     )
-    if function_definitions:
-        function_definitions = "\n" + function_definitions + "\n\n"
-    main_call_str = in_printer_program.initial_call_str(
-        atom_to_code, function_call_str
-    )
-    if main_call_str:
-        main_call_str = "\n  " + main_call_str[0:-1]
 
-    res = (
+
+def _join_to_final(main_call, function_definitions):
+    function_definitions_str = (
+        "\n" + "\n\n\n".join(function_definitions) + "\n\n"
+        if function_definitions
+        else ""
+    )
+    return (
         "program Main;\n"
-        + function_definitions
+        + function_definitions_str
         + "\nbegin"
-        + main_call_str
+        + main_call
         + "\nend.\n"
     )
-    return res
 
 
-def proc(in_str, gen_function_names=None):
-    """
-    returns a Pascal code printing in_str to the standard output
-    """
-    if gen_function_names is None:
-        gen_function_names = core.gen_function_names("P")
-
-    printer_program = core.get_printer_program(in_str, gen_function_names)
-    return proc_printer_program(printer_program)
+proc_printer_program, proc = utils.get_all_proc_functions(
+    _main_call_to_code, function_to_code, _join_to_final
+)
