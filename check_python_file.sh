5551647b6beefcf3fb9616f7306c416cd4f1ef35
“vil02” <vil02@o2.pl>
2023-01-05 11:35:01 +0100

    style: linting

--- check_python_file.sh
+++ check_python_file.sh
@@ -2,30 +2,20 @@
 
 set -euo pipefail
 
-function check_single()
-{
-    declare -r in_path="$1"
-    declare -i result_val=0
-    printf "Checking \"%s\"\n" "${in_path}"
-    printf "Checking with pylint:\n"
-    if ! poetry run pylint "${in_path}" ; then
-        result_val=1
-    fi
-
-    printf "Checking with flake8:\n"
-    if ! poetry run flake8 "${in_path}" --count --max-line-length=80 --show-source ; then
-        result_val=1
-    fi
-    printf "...done\n\n\n"
-    return "${result_val}"
-}
-
-
 declare -i exit_code=0
 
 for cur_file in "$@"
 do
-    check_single "${cur_file}" || exit_code=1
+    printf "Checking \"%s\"\n" "${cur_file}"
+    printf "Checking with pylint:\n"
+    if ! poetry run pylint "${cur_file}" ; then
+        exit_code=1
+    fi
+
+    printf "Checking with flake8:\n"
+    if ! poetry run flake8 "${cur_file}" --count --max-line-length=80 --show-source ; then
+        exit_code=1
+    fi
 done
 
 if [[ ${exit_code} -eq 0 ]] ; then
