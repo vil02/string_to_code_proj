daafaab025a5cdc0f9b0b6a2e8ee46d04efbcbf2
piotr.idzik <vil02@o2.pl>
2023-09-24 11:06:42 +0200

    docs: add examples illustrating usage of general `string_to_code`

---
+++ examples/example_general.py
@@ -0,0 +1,14 @@
+"""
+this file illustrates the usage of the general string_to_code module
+"""
+import setup_examples  # noqa # pylint: disable=unused-import
+from string_to_code import string_to_code
+
+EXAMPLE_STR = "Hello, World!"
+TARGET_LANGUAGE = "ada"
+
+assert TARGET_LANGUAGE in string_to_code.get_target_languages()
+
+CODE = string_to_code.proc(TARGET_LANGUAGE, EXAMPLE_STR)
+
+print(f"{TARGET_LANGUAGE} code below prints '{EXAMPLE_STR}':\n{CODE}")
