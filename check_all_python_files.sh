476ac27bac9fd6ba8899a6bbea0a9f7fd42e2d1f
vil02 <65706193+vil02@users.noreply.github.com>
2025-01-02 16:48:16 +0100

    style: simplify static analysis of python files

--- check_all_python_files.sh
+++ check_all_python_files.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+poetry run ruff check .
+poetry run mypy .
 poetry run bandit -c bandit.yml -r .
 
 find . -name "*.py" -not -path "./tests/example_data/python3/*" -exec ./check_python_file.sh {} +
